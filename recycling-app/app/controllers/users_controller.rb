f8bc66a711bf52142966fd747a5e26cb5775dbe9
Sree <mesreevidhya@gmail.com>
2019-04-30 13:44:36 +1000

    authorisation debugging

---
+++ recycling-app/app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def people
+  end
+  def business
+  end
+  def admin
+  end
+end
--- recycling-app/db/schema.rb
+++ recycling-app/db/schema.rb
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2019_04_29_044147) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "people"
+    t.boolean "business"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
