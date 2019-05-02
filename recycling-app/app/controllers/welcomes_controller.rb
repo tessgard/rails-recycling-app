dd5ab6d4db79d1640c61b3debd0cf56c1e040416
Tess <gard.tess@gmail.com>
2019-05-02 14:37:51 +1000

    added standard category images and logic to assign image according to user input

--- recycling-app/app/controllers/welcomes_controller.rb
+++ recycling-app/app/controllers/welcomes_controller.rb
@@ -1,8 +1,19 @@
 class WelcomesController < ApplicationController
   def index
     @products = Product.all.sample(5)
+    @image_name = ''
+
+    @products.each do |product|
+      if product.category == "Glass"
+        @image_name = "glass.jpg"
+      elsif product.category == "Cardboards"
+        @image_name = "cardboard.jpg"
+      end
+    end
 
   end
+
+
   def people_dashboard 
     @user = current_user
     @products = Product.where(user_id: current_user.id)
@@ -17,4 +28,5 @@ class WelcomesController < ApplicationController
     redirect_to my_dashboard_path
     super
   end
+
 end
--- recycling-app/app/models/product.rb
+++ recycling-app/app/models/product.rb
@@ -3,5 +3,13 @@ class Product < ApplicationRecord
   references :business, index: true
   has_one_attached :uploaded_image
   belongs_to :user
-  
+
+  def find_image_name
+      if self.category == "Glass"
+        image_name = "glass.jpg"
+      elsif self.category == "Cardboards"
+        image_name = "cardboard.jpg"
+      end
+      image_name
+  end
 end
