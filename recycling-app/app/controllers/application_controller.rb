9611a388dc7708bb12e90b58999a96e18e8a40a3
Tess <gard.tess@gmail.com>
2019-05-07 15:55:33 +1000

    debugging

--- recycling-app/app/controllers/application_controller.rb
+++ recycling-app/app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   # def after_sign_in_path_for(people)
   #   redirect_to my_dashboard_path
   # end
--- recycling-app/db/schema.rb
+++ recycling-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_051922) do
+ActiveRecord::Schema.define(version: 2019_05_07_045939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2019_05_03_051922) do
     t.datetime "updated_at", null: false
     t.boolean "deactivated"
     t.string "name"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
