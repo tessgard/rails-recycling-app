62853c2995d55b1bc2de7421f99994ab35ce7e89
Sree <mesreevidhya@gmail.com>
2019-05-08 16:34:36 +1000

    Added business name

--- recycling-app/db/schema.rb
+++ recycling-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_020509) do
+ActiveRecord::Schema.define(version: 2019_05_08_060352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_05_08_020509) do
     t.datetime "updated_at", null: false
     t.boolean "deactivated"
     t.boolean "admin"
+    t.string "name"
     t.index ["email"], name: "index_businesses_on_email", unique: true
     t.index ["reset_password_token"], name: "index_businesses_on_reset_password_token", unique: true
   end
