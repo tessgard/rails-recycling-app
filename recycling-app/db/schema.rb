6a76dee89daa45faa4a5029e5310d5060fd42787
Tess <gard.tess@gmail.com>
2019-05-09 14:35:02 +1000

    debugging an error on the layout application file

--- recycling-app/db/schema.rb
+++ recycling-app/db/schema.rb
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2019_05_08_060352) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "deactivated"
-    t.boolean "admin"
     t.string "name"
     t.index ["email"], name: "index_businesses_on_email", unique: true
     t.index ["reset_password_token"], name: "index_businesses_on_reset_password_token", unique: true
